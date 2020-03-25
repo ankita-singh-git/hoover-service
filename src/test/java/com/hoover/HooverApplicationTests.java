29f01a3ad2b52906c793aad624ee3e8fad46331c
ankitasingh12 <ankita.singh@level.global>
2020-03-25 16:59:06 +0000

    Initial project setup

---
+++ src/test/java/com/hoover/HooverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
